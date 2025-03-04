cbfe76a59dee763c465e8d30ca7bb952065297cf
Foysal-Munsy <bdfoysal555@gmail.com>
2025-03-04 16:51:11 +0600

    val added

--- js/script.js
+++ js/script.js
@@ -11,3 +11,31 @@ date.innerText = new Date()
     year: "numeric",
   })
   .replace(",", "");
+
+// Bg Color
+const button = document.getElementById("bg-btn");
+button.addEventListener("click", function () {
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
+});
+
+// Marked as completed
+const completedBtn = document.querySelectorAll(".completed-btn");
+for (const btn of completedBtn) {
+  btn.addEventListener("click", function () {
+    alert("Tasked marked as completed!");
+    btn.disabled = true;
+    btn.classList.add("bg-gray-400");
+    let first = document.getElementById("task-assigned");
+    // console.log(first);
+    let val = parseInt(first.innerText);
+    --val;
+    first.innerHTML = "0" + val;
+
+    let last = document.getElementById("total-task");
+    let vall = parseInt(last.innerText);
+    // console.log(vall);
+    ++vall;
+    last.innerHTML = vall;
+  });
+}
